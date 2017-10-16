920aa8a45d1c70b498731ab18db551795a7f4284
System <none>
2017-10-16 11:41:40 +0000

    🏔🔑 Updated with Glitch

--- app.js
+++ app.js
@@ -113,16 +113,18 @@ app.use('/',routes);
  //});
 
 
-
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 // error handler
-app.use(function(err, req, res, next) {
+//app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ // res.locals.message = err.message;
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  //res.status(err.status || 500);
+  //res.render('error');
+//});
 
 module.exports = app;
